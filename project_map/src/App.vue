18f3688538da8c4b8a768157df3930557b6d3d8e
Okina <nao.xsrz2@gmail.com>
2024-05-17 18:29:17 +0900

    dendai_coffe

--- project_map/src/App.vue
+++ project_map/src/App.vue
@@ -1,32 +1,92 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-app-bar elevation="2">
+      <RouterLink to="/about">About</RouterLink>
+    </v-app-bar>
+    <v-main>
+      <v-card
+      max-width="10000"
+      >
+        <v-img
+        height="250"
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <v-toolbar-title>タイトル</v-toolbar-title>
+        <v-card-title>電大カフェ</v-card-title>
+        <v-card-text>
+          <div class="my-4 text-subtitle-1">
+            作品：東京電機大学
+          </div>
+          <div class="my-4 text-subtitle-1">
+            〒120-8551 東京都足立区千住旭町５
+          </div>
+          <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+        </v-card-text>
+      </v-card>
+      <div>
+        <div class="map" ref="googleMap" />
+      </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+      <div>
+        ルートを表示
+      </div>
+    </v-main>
 
-  <v-sheet id="map"
-      class="pa-0"
-      :light="true"
-      style="width: 100%; height: 300px;"
+    <v-footer
+      color="primary lighten-1"
+      padless
     >
-      google map
-  </v-sheet>
-
-  <RouterView />
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-col
+          class="primary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>情報通信プロジェクト</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
+  
 </template>
 
+<script>
+import GoogleMapsApiLoader from 'google-maps-api-loader';
+import { RouterLink, RouterView } from 'vue-router'
 
+export default {
+  name: 'Map',
+  data() {
+    return {
+      google: null,
+      mapConfig: {
+        center: {
+          lat: 35.74861780559465, 
+          lng: 139.80627656294405
+        },
+        zoom: 17
+      }
+    }
+  },
+  async mounted() {
+    this.google = await GoogleMapsApiLoader({
+      apiKey: 'AIzaSyB6PSkvWZX2a3x96hmrjJIbnZGDQwhkC6I'
+    });
+    this.initializeMap();
+  },
+  methods: {
+    initializeMap() {
+      new this.google.maps.Map(this.$refs.googleMap, this.mapConfig);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.map {
+  width: 80vw;
+  height: 50vh;
+}
+</style>
